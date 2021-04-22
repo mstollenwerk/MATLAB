a90857112bb15a87f0a540fa06c02ee757a92f4e
mstollenwerk <michael.stollenwerk@live.com>
2021-04-22 14:50:04 +0200

    Created fork_files.m from path_copy.m

---
+++ toolboxes_own/utility/fork_files.m
@@ -0,0 +1,10 @@
+function fork_files(files_, rootdir, destdir)
+
+for ii = 1:length(filedirs)
+    cpdir = strcat(rootdir,files_{ii});
+    dest = strcat(destdir,files_{ii});
+    mkdir(dest)
+    copyfile(cpdir, dest)
+end
+
+end
