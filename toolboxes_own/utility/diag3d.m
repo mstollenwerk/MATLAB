2816c625961bf8794ed4891a4de17f70040703ef
mstollenwerk <michael.stollenwerk@live.com>
2021-04-25 15:25:19 +0200

    Update diag3d.m

--- toolboxes_own/utility/diag3d.m
+++ toolboxes_own/utility/diag3d.m
@@ -1,11 +1,21 @@
 function y = diag3d(X)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
-[p,~,N] = size(X);
 
-y = NaN(N,p);
-for ii = 1:N
-    y(ii,:) = diag(X(:,:,ii));
+if numel(size(X))==3
+    [p,~,N] = size(X);
+
+    y = NaN(N,p);
+    for ii = 1:N
+        y(ii,:) = diag(X(:,:,ii));
+    end
+elseif numel(size(X))==2
+    [N,p] = size(X);
+
+    y = NaN(p,p,N);
+    for ii = 1:N
+        y(:,:,ii) = diag(X(ii,:));
+    end    
 end
 
 end
