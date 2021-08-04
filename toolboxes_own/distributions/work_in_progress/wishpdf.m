f9a282d9f80a5c7b6324a2171df33965841c259b
mstollenwerk <michael.stollenwerk@live.com>
2021-08-04 15:40:41 +0200

    Reverted "Undo Gitignore" Commit

--- toolboxes_own/distributions/work_in_progress/wishpdf.m
+++
@@ -1,9 +0,0 @@
-function y = wishpdf(X,S,dof)
-
-k=size(S,1);
-m=1;
-for i=1:k
-    m=m*gamma((dof+1-i)/2);
-end
-y=det(X)^((dof-k-1)/2)*exp(-1/2*trace(S\X))/(pi^(k*(k-1)/4)*2^(dof*k/2)*m*det(S)^(dof/2));
-end
