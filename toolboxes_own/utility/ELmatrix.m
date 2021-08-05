7007d5f2f6410f13b824a4d86a6757d5473c8008
mstollenwerk <michael.stollenwerk@live.com>
2021-08-05 11:49:40 +0200

    Update ELmatrix.m

--- toolboxes_own/utility/ELmatrix.m
+++ toolboxes_own/utility/ELmatrix.m
@@ -9,11 +9,23 @@ function [ L ] = ELmatrix( n )
 % 
 % L=blkdiag(storage{:});  
 
-n_ = n*(n+1)/2;
-IndexMatrix = reshape(1:n^2,n,n);
-select = tril(true(n));
-v = IndexMatrix(select);
+persistent C_ELmatrix
+if isempty(C_ELmatrix)
+   nCache = 1000;  % Set according to your needs
+   C_ELmatrix      = cell(1, nCache);
+end
+if n <= numel(C_ELmatrix) && ~isempty(C_ELmatrix{n})
+    L = C_ELmatrix{n};
+else
+    n_ = n*(n+1)/2;
+    IndexMatrix = reshape(1:n^2,n,n);
+    select = tril(true(n));
+    v = IndexMatrix(select);
 
-L = sparse(1:n_, v, 1, n_, n^2);
-  
+    L = sparse(1:n_, v, 1, n_, n^2);
+    if n <= numel(C_ELmatrix)
+      C_ELmatrix{n} = L;
+    end
+end
+
 end
