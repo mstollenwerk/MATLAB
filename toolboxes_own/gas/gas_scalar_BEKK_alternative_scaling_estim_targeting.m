b6820bd695f89757c8042ff2c73c421fe749a898
mstollenwerk <michael.stollenwerk@live.com>
2021-08-06 13:00:01 +0200

    Alternative GAS Score Scaling

---
+++ toolboxes_own/gas/gas_scalar_BEKK_alternative_scaling_estim_targeting.m
@@ -0,0 +1,199 @@
+function [eparam, tstats, logL, fit, fcst, optimoutput] = ...
+	gas_scalar_BEKK_alternative_scaling_estim_targeting( X, p, q, dist, x0, varargin )
+%
+%
+% Michael Stollenwerk
+% michael.stollenwerk@live.com
+% 25.03.2021
+
+%% Input Checking
+% Will be added later
+[k,~,T] = size(X);
+k_ = k*(k+1)/2;
+if ~isempty(varargin) && strcmp(varargin{1},'OptimizeOrdering')
+    optimize_ordering = 1;
+    if numel(varargin)~=1
+        varargin = varargin(2:end);
+    end
+else
+    optimize_ordering = 0;
+end
+%% Optimization
+obj_fun = @(param) obj_fun_wrapper(param, X, p, q, dist);
+% x0-----------------------------------------------------------------------
+if isempty(x0)
+    x0 = [zeros(1,2*p) ones(1,q)*.75/q];
+    if strcmp( dist, 'Wish' )
+		x0 = [ x0, 2*k ]';
+    elseif strcmp( dist, 'LaplaceWish' )
+		x0 = [ x0, 2*k ]';        
+    elseif strcmp( dist, 'iWish' )
+		x0 = [ x0, 2*k ]';  
+    elseif strcmp( dist, 'tWish' )
+		x0 = [ x0, 2*k, 5 ]';
+    elseif strcmp( dist, 'itWish' )
+		x0 = [ x0, 2*k, 5 ]';
+    elseif strcmp( dist, 'F' )
+		x0 = [ x0, 2*k, 2*k ]';        
+    elseif strcmp( dist, 'Riesz' )
+		x0 = [ x0, ones(1,k).*(2*k) ]';
+    elseif strcmp( dist, 'iRiesz' )
+		x0 = [ x0, ones(1,k).*(2*k) ]';         
+    elseif strcmp( dist, 'tRiesz' )
+		x0 = [ x0, ones(1,k).*(2*k), 5 ]'; 
+    elseif strcmp( dist, 'itRiesz' )
+		x0 = [ x0, ones(1,k).*(2*k), 5 ]';    
+    elseif strcmp( dist, 'FRiesz' )
+		x0 = [ x0, ones(1,k).*(2*k), ones(1,k).*(2*k) ]';        
+    end
+end
+% Restrictions-------------------------------------------------------------
+if strcmp( dist, 'Wish' )
+    lb = [-inf(2*p+q,1); k-1];
+elseif strcmp( dist, 'LaplaceWish' )
+    lb = [-inf(2*p+q,1); k-1];    
+elseif strcmp( dist, 'iWish' )
+    lb = [-inf(2*p+q,1); k+1];
+elseif strcmp( dist, 'F' )
+    lb = [-inf(2*p+q,1); k+1; k-1];
+elseif strcmp( dist, 'tWish' )
+    lb = [-inf(2*p+q,1); k-1; 2];
+elseif strcmp( dist, 'itWish' )
+    lb = [-inf(2*p+q,1); k+1; 0];    
+elseif strcmp( dist, 'Riesz' )
+    lb = [-inf(2*p+q,1)', 0:k-1]';  
+elseif strcmp( dist, 'iRiesz' )
+    lb = [-inf(2*p+q,1)', 2:k+1]';  
+elseif strcmp( dist, 'tRiesz' )
+    lb = [-inf(2*p+q,1)', 0:k-1, 2]'; 
+elseif strcmp( dist, 'itRiesz' )
+    lb = [-inf(2*p+q,1)', 0:k-1, 2]';
+elseif strcmp( dist, 'FRiesz' )
+    lb = [-inf(2*p+q,1)', (0:k-1), (k-(1:k)+2)]';    
+end
+A = [ zeros(1,2*p) ones(1,q) zeros(1, length(x0)-2*p-q) ];   % Stationarity
+b = 1;                                                   % Stationarity
+% Optimization-------------------------------------------------------------
+disp('-------------------------------------------------------------------')
+disp('-------------------------------------------------------------------')
+disp(strcat("Starting gas scalar BEKK(",num2str(p),",",num2str(q),")-",dist," estimation, targeting."))
+disp('-------------------------------------------------------------------')
+disp('-------------------------------------------------------------------')
+[eparam,optimoutput] = ...
+	my_fmincon_robust(...
+		obj_fun, ...
+		x0, ...
+        lb,[], ...
+        [2*p+q, numel(x0)]', ...
+        1e-2,...
+		varargin{:} ...
+	);
+% Implementing an algo for permuting order of assets akin to Blasques et
+% al.
+if optimize_ordering
+    
+    perm_improvement = inf;
+    while perm_improvement > 0
+        rng(1)        
+        for ii = 1:100
+            perm_(ii,:) = randperm(k);
+        end
+        perm_ = unique(perm_, 'rows', 'stable');
+        for ii = 1:size(perm_,1)
+            nLogL_permuted_assets(ii) = ...
+                obj_fun_wrapper( eparam, X(perm_(ii,:),perm_(ii,:),:), p, q, dist);
+        end
+        [perm_min_nLogL,min_ii] = min(nLogL_permuted_assets);
+        % If min is smaller than current nLogL, permute assets and let
+        % solver run again, update current nLogL, save permutation.
+        perm_improvement = -perm_min_nLogL + optimoutput{end}.history.fval(end);
+        if perm_improvement > 0
+            perm_improvement
+            obj_fun = @(param) obj_fun_wrapper( param, X(perm_(min_ii,:),perm_(min_ii,:),:), p, q, dist);
+            [eparam,optimoutput] = ...
+                my_fmincon_robust(...
+                    obj_fun, ...
+                    eparam, ...
+                    lb,[], ...
+                    [p+q, numel(x0)]', ...
+                    1e-2,...
+                    varargin{:} ...
+                );
+            optimoutput{end}.perm_ = perm_(min_ii,:);
+        end
+        clear perm_ nLogL_permuted_assets
+    end
+    
+end
+if ~isfield(optimoutput{end},'perm_')
+    optimoutput{end}.perm_ = 1:k;
+end
+disp('-------------------------------------------------------------------')
+disp('-------------------------------------------------------------------')
+disp(strcat("Finished gas scalar BEKK(",num2str(p),",",num2str(q),")-",dist," estimation, targeting."))
+disp('-------------------------------------------------------------------')
+disp('-------------------------------------------------------------------')
+% [eparam,optimoutput] = ...
+% 	my_fmincon(...
+% 		obj_fun, ...
+% 		x0, ...
+%         A,b,[],[],lb,[],[], ...
+% 		varargin{:} ...
+% 	);
+% while strcmp(optimoutput.message(50:111),'fmincon stopped because the gradient calculation is undefined.')
+%     x0_new = eparam.all;
+%     x0_new(p+1:q) = x0_new(p+1:q)*.9;
+%     [eparam,optimoutput] = ...
+%         my_fmincon(...
+%             obj_fun, ...
+%             x0_new, ...
+%             A,b,[],[],lb,[],[], ...
+%             varargin{:} ...
+%         );
+% end
+% Tstats-------------------------------------------------------------------
+%[VCV,A,B,scores,hess,gross_scores] = robustvcv(fun, eparam, 3);
+[VCV,scores,gross_scores] = vcv( obj_fun, eparam );
+tstats = eparam./sqrt(diag(VCV));
+
+% Output Creation----------------------------------------------------------
+[ nLogL, logLcontr, Sigma_, ScaledScore, eparam ] = obj_fun( eparam );
+
+aic = 2*nLogL + 2*(numel(x0)+k_);
+bic = 2*nLogL + log(T)*(numel(x0)+k_); % see Yu, Li and Ng (2017) 
+logL = struct(...
+    'logL', -nLogL,...
+    'aic', aic,...
+    'bic', bic,...
+    'logLcontr', logLcontr...
+);
+
+iperm_ = iperm(optimoutput{end}.perm_);
+Sigma_ = Sigma_(iperm_,iperm_,:);
+ScaledScore = ScaledScore(iperm_,iperm_,:);
+fit = struct( ...
+    'Sigma_', Sigma_(:,:,1:T), ...
+    'ScaledScore', ScaledScore ...
+);
+fcst = struct('Sigma_', Sigma_(:,:,T+1:end));
+
+end
+
+function [ nLogL, logLcontr, Sigma_, ScaledScore, param, fitplot ] = obj_fun_wrapper(param, X, p, q, dist) 
+
+    if sum(param(p+1:p+q)) >= 1
+        nLogL = inf;   
+        return
+    end
+    
+    vechcholSig = vechchol( mean(X,3)*(1-sum(param(p+1:p+q))) );
+    
+    [ nLogL, logLcontr, Sigma_, ScaledScore, param, fitplot ] = gas_scalar_BEKK_alternative_scaling_likeRec( ...
+        [vechcholSig; param], ...
+        p, ...
+        q, ...
+        X, ...
+        dist ...
+    );
+
+end
---
+++ toolboxes_own/gas/gas_scalar_BEKK_alternative_scaling_likeRec.m
@@ -0,0 +1,202 @@
+function [ nLogL, logLcontr, SigmaE, ScaledScore1, varargout ] = ...
+    gas_scalar_BEKK_alternative_scaling_likeRec( param, p, q, X, dist )
+%
+% Michael Stollenwerk
+% michael.stollenwerk@live.com
+% 11.08.2020
+
+[k,~,T] = size(X);
+k_ = k*(k+1)/2;
+%% Parameters
+intrcpt = ivechchol(param(1:k_));
+scoreparam1 = param(k_ + 1 : k_ + p);
+scoreparam2 = param(k_ + p + 1 : k_ + 2*p);
+garchparam = param(k_ + 2*p : k_+ 2*p + q);
+
+if strcmp( dist, 'Wish' )
+    n = param(k_+ 2*p + q + 1);    
+elseif strcmp( dist, 'iWish' )
+    n = param(k_+ 2*p + q + 1);
+elseif strcmp( dist, 'LaplaceWish' )
+    n = param(k_+ 2*p + q + 1);      
+elseif strcmp( dist, 'tWish' )
+    n = param(k_+ 2*p + q + 1); 
+    nu = param(k_+ 2*p + q + 2);
+elseif strcmp( dist, 'itWish' )
+    n = param(k_+ 2*p + q + 1); 
+    nu = param(k_+ 2*p + q + 2);
+elseif strcmp( dist, 'F' )
+    n = param(k_+ 2*p + q + 1); 
+    nu = param(k_+ 2*p + q + 2);
+elseif strcmp( dist, 'Riesz' )
+    n = param(k_+ 2*p + q + 1 : k_+ 2*p + q + k);    
+elseif strcmp( dist, 'iRiesz' )
+    n = param(k_+ 2*p + q + 1 : k_+ 2*p + q + k);  
+elseif strcmp( dist, 'tRiesz' )
+    n = param(k_+ 2*p + q + 1 : k_+ 2*p + q + k); 
+    nu = param(k_+ p + q + k + 1);
+elseif strcmp( dist, 'itRiesz' )
+    n = param(k_+ 2*p + q + 1 : k_+ 2*p + q + k); 
+    nu = param(k_+ 2*p + q + k + 1);
+elseif strcmp( dist, 'FRiesz' )
+    n = param(k_+ 2*p + q + 1 : k_+ 2*p + q + k); 
+    nu = param(k_+ 2*p + q + k + 1 : k_+ 2*p + q + k + k);        
+end
+
+if nargout >= 5
+    param_out.intrcpt = intrcpt;
+    param_out.scoreparam1 = scoreparam1;
+    param_out.scoreparam2 = scoreparam2;
+    param_out.garch_param = garchparam;
+    param_out.n = n;
+    
+    if exist('nu','var')
+        param_out.nu = nu;
+    end
+
+    param_out.all = param;
+    
+    varargout{1} = param_out;
+end
+%% Data Storage
+SigmaE = NaN(k,k,T+22);
+ScaledScore1 = NaN(k,k,T);
+ScaledScore2 = NaN(k,k,T);
+logLcontr = NaN(T,1);
+%% Recursion
+% Initialize with Backcast. Code copied from Sheppard MFE Toolbox.
+m = ceil(sqrt(T));
+w = .06 * .94.^(0:(m-1));
+w = reshape(w/sum(w),[1 1 m]);
+backCast = sum(bsxfun(@times,w,X(:,:,1:m)),3);
+ini = backCast;
+% Initialize recursion at unconditional mean (stationarity assumed).
+% ini = intrcpt./(1 - sum(garchparam));
+G = Dmatrix(k);
+iG = (G'*G)\G';
+L = ELmatrix(k);
+I = eye(k);
+for tt=1:T
+    SigmaE(:,:,tt) = intrcpt;
+    for jj = 1:p
+        if (tt-jj) > 0
+            SigmaE(:,:,tt) = SigmaE(:,:,tt) ...
+                                + scoreparam1(jj)*ScaledScore1(:,:,tt-jj) ...
+                                + scoreparam2(jj)*ScaledScore2(:,:,tt-jj);
+        end
+    end
+    for jj = 1:q
+        if (tt-jj) <= 0
+            SigmaE(:,:,tt) = SigmaE(:,:,tt) + garchparam(jj)*ini;
+        else
+            SigmaE(:,:,tt) = SigmaE(:,:,tt) + garchparam(jj)*SigmaE(:,:,tt-jj);
+        end
+    end
+    
+    try
+        % Centering distributions
+        if strcmp( dist, 'Wish' )
+            Y = n;
+            Sigma_ = SigmaE(:,:,tt)/Y;
+            dSigmaEdSigma = Y;
+        elseif strcmp( dist, 'LaplaceWish' )
+            Y = n;
+            Sigma_ = SigmaE(:,:,tt)/Y;
+            dSigmaEdSigma = Y;
+        elseif strcmp( dist, 'iWish' )
+            Y = 1/(n-k-1);
+            Sigma_ = SigmaE(:,:,tt)/Y;
+            dSigmaEdSigma = Y;
+        elseif strcmp( dist, 'tWish')
+            Y = n*nu/(nu-2);
+            Sigma_ = SigmaE(:,:,tt)/Y;
+            dSigmaEdSigma = Y;
+        elseif strcmp( dist, 'itWish')
+            Y = 1/(n-k-1);
+            Sigma_ = SigmaE(:,:,tt)/Y;
+            dSigmaEdSigma = Y;
+        elseif strcmp( dist, 'F' )
+            Y = n/(nu-k-1);
+            Sigma_ = SigmaE(:,:,tt)/Y;
+            dSigmaEdSigma = Y;
+        elseif strcmp( dist, 'Riesz' )
+            Y = diag(n);
+            CsigE = chol(SigmaE(:,:,tt),'lower');
+            C = CsigE/sqrtm(Y);
+            Sigma_ = C*C';
+            dSigmaEdSigma = G'*kron(C*Y,I)*L'/(G'*kron(C,I)*L')*(G'*G);
+        elseif strcmp( dist, 'iRiesz' )
+            Y = inv(diag(n-k-1));
+            iCdotSigE = inv(chol(inv(SigmaE(:,:,tt)),'lower'));
+            iCdot = iCdotSigE/sqrtm(Y);
+            Sigma_ = iCdot'*iCdot;
+            dSigmaEdSigma = G'*kron(iCdot',iCdot'*Y*iCdot)*L'/(G'*kron(iCdot',Sigma_)*L')*(G'*G);
+        elseif strcmp( dist, 'tRiesz' )
+            Y = diag(n).*nu./(nu - 2);
+            CsigE = chol(SigmaE(:,:,tt),'lower');
+            C = CsigE/sqrtm(Y);
+            Sigma_ = C*C';
+            dSigmaEdSigma = G'*kron(C*Y,I)*L'/(G'*kron(C,I)*L')*(G'*G);
+        elseif strcmp( dist, 'itRiesz' )
+            Y = inv(diag(n-k-1));
+            iCdotSigE = inv(chol(inv(SigmaE(:,:,tt)),'lower'));
+            iCdot = iCdotSigE/sqrtm(Y);
+            Sigma_ = iCdot'*iCdot;
+            dSigmaEdSigma = G'*kron(iCdot',iCdot'*Y*iCdot)*L'/(G'*kron(iCdot',Sigma_)*L')*(G'*G);
+        elseif strcmp( dist, 'FRiesz' )
+            Y = matvFRieszexpmat(n,nu);
+            CsigE = chol(SigmaE(:,:,tt),'lower');
+            C = CsigE/sqrtm(Y);
+            Sigma_ = C*C';
+            dSigmaEdSigma = G'*kron(C*Y,I)*L'/(G'*kron(C,I)*L')*(G'*G);
+        end
+        % Likelihood Evaluation    
+        if exist('nu','var')
+            [logLcontr(tt), score, ~, ~, ~] = logpdf( dist, X(:,:,tt), Sigma_, n, nu );                           %%%%%%%%%%%%%%%%%%%%%%%
+        else
+            [logLcontr(tt), score, ~, ~, ~] = logpdf( dist, X(:,:,tt), Sigma_, n );
+        end
+    catch ME
+%         ME.message
+%         [ME.stack.line]'
+%         {ME.stack.name}'
+        nLogL = inf;
+        logLcontr = NaN;
+        varargout{1} = NaN;      
+        varargout{2} = NaN;
+        return
+    end
+    
+    % Scaled Score 
+    ScaledScore1(:,:,tt) = ivech(iG*kron2(Sigma_)*iG'*(dSigmaEdSigma\score.Sigma_'));
+    ScaledScore2(:,:,tt) = ivech(iG*vec2(Sigma_)*iG'*(dSigmaEdSigma\score.Sigma_'));
+    
+end
+%% Fcst
+for tt=T+1:T+22
+    SigmaE(:,:,tt) = intrcpt;
+    for jj = 1:p
+        if (tt-jj) <= T
+            SigmaE(:,:,tt) = SigmaE(:,:,tt) ...
+                                + scoreparam1(jj)*ScaledScore1(:,:,tt-jj) ...
+                                + scoreparam2(jj)*ScaledScore2(:,:,tt-jj);
+        end
+    end
+    for jj = 1:q
+        SigmaE(:,:,tt) = SigmaE(:,:,tt) + garchparam(jj)*SigmaE(:,:,tt-jj);
+    end
+end
+%% Log-Likelihood
+nLogL = -sum(logLcontr);
+%% Fit-Plot(s)
+if nargout >= 6
+    fitplot = NaN;
+%     fitplot = figure("Visible",false,"WindowState",'fullscreen');
+%     y1 = sum(diag3d(X),2);
+%     y2 = sum(diag3d(SigmaE(:,:,1:T)),2);
+%     plot(y1);
+%     hold on
+%     plot(y2,'LineWidth',2)
+%     text(T/2,max(y1)*3/4,strcat(num2str(sum(logLcontr))," | Score-Parameters:", num2str(scoreparam), " | Garch-Parameters:", num2str(garchparam), " | Matrix Euclidean Distance:", num2str(mean(matrix_euclidean_loss(X,SigmaE(:,:,1:T))))));
+    varargout{2} = fitplot;
+end
