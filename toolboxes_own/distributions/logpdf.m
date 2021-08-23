b9d81b500b4bb5f41b71dc41267fbf880828a3df
mstollenwerk <michael.stollenwerk@live.com>
2021-08-23 13:55:39 +0200

    Revert "logpdf.m, aligning nargout with requested output arguments from liklihoods"
    This reverts commit 67d5dd3a073bb616743888fd1e225256ed08b215.

--- toolboxes_own/distributions/logpdf.m
+++ toolboxes_own/distributions/logpdf.m
@@ -23,30 +23,33 @@ if nargout == 1
         y = matvFRieszlike(varargin{:}, dta);
     end
 y = -y;
-elseif nargout==2
+elseif nargout>=2
     if strcmp( dist, 'Wish' )
-        [y, ~, score] = matvWishlike(varargin{:}, dta);
+        [y, ~, score, hessian, param, fisherinfo] = matvWishlike(varargin{:}, dta);
     elseif strcmp( dist, 'iWish' )
-        [y, ~, score] = matviWishlike(varargin{:}, dta);
+        [y, ~, score, hessian, param, fisherinfo] = matviWishlike(varargin{:}, dta);
     elseif strcmp( dist, 'tWish' )
-        [y, ~, score] = matvtWishlike(varargin{:}, dta);
+        [y, ~, score, hessian, param, fisherinfo] = matvtWishlike(varargin{:}, dta);
     elseif strcmp( dist, 'itWish' )
-        [y, ~, score] = matvitWishlike(varargin{:}, dta);
+        [y, ~, score, hessian, param, fisherinfo] = matvitWishlike(varargin{:}, dta);
     elseif strcmp( dist, 'F' )
-        [y, ~, score] = matvFlike(varargin{:}, dta);
+        [y, ~, score, hessian, param, fisherinfo] = matvFlike(varargin{:}, dta);
     elseif strcmp( dist, 'Riesz' )
-        [y, ~, score] = matvRieszlike(varargin{:}, dta);
+        [y, ~, score, hessian, param, fisherinfo] = matvRieszlike(varargin{:}, dta);
     elseif strcmp( dist, 'iRiesz' )
-        [y, ~, score] = matviRieszlike(varargin{:}, dta);
+        [y, ~, score, hessian, param, fisherinfo] = matviRieszlike(varargin{:}, dta);
     elseif strcmp( dist, 'tRiesz' )
-        [y, ~, score] = matvtRieszlike(varargin{:}, dta);
+        [y, ~, score, hessian, param, fisherinfo] = matvtRieszlike(varargin{:}, dta);
     elseif strcmp( dist, 'itRiesz' )
-        [y, ~, score] = matvitRieszlike(varargin{:}, dta);
+        [y, ~, score, hessian, param, fisherinfo] = matvitRieszlike(varargin{:}, dta);
     elseif strcmp( dist, 'FRiesz' )
-        [y, ~, score] = matvFRieszlike(varargin{:}, dta);
+        [y, ~, score, hessian, param, fisherinfo] = matvFRieszlike(varargin{:}, dta);
     end
     y = -y;
     varargout{1} = score;
+    varargout{2} = hessian;
+    varargout{3} = param;
+    varargout{4} = fisherinfo;
 end
 
 end
