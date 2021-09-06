8bc9bacea56c444eefd62ea7746714e57849ff8f
mstollenwerk <michael.stollenwerk@live.com>
2021-09-06 11:14:17 +0200

    switched caicov scalar BEKK to non centered Sigma_ recursion (as i did b4 for gas)

--- toolboxes_own/caicov/caicov_scalar_BEKK_estim_targeting.m
+++ toolboxes_own/caicov/caicov_scalar_BEKK_estim_targeting.m
@@ -21,28 +21,51 @@ end
 %% Optimization
 obj_fun = @(param) obj_fun_wrapper(param, X, p, q, dist);
 % x0-----------------------------------------------------------------------
-if isempty(x0)
-    x0 = [ones(1,p)*.01/p ones(1,q)*.75/q];
+if isempty(x0) || (~isempty(x0) && (isinf(obj_fun(x0)) || isnan(obj_fun(x0))))
     if strcmp( dist, 'Wish' )
-		x0 = [ x0, 2*k ]';
+		x0_df = 2*k;
     elseif strcmp( dist, 'iWish' )
-		x0 = [ x0, 2*k ]';  
+		x0_df = 2*k;  
     elseif strcmp( dist, 'tWish' )
-		x0 = [ x0, 2*k, 5 ]';
+		x0_df = [ 2*k, 5 ];
     elseif strcmp( dist, 'itWish' )
-		x0 = [ x0, 2*k, 5 ]';
+		x0_df = [ 2*k, 5 ];
     elseif strcmp( dist, 'F' )
-		x0 = [ x0, 2*k, 2*k ]';        
+		x0_df = [ 2*k+3, 2*k+3 ];  % nu > p - 3 for Fisher Info to exist.
     elseif strcmp( dist, 'Riesz' )
-		x0 = [ x0, ones(1,k).*(2*k) ]';
+		x0_df = ones(1,k).*(2*k);
+    elseif strcmp( dist, 'Riesz2' )
+		x0_df = ones(1,k).*(2*k);
     elseif strcmp( dist, 'iRiesz' )
-		x0 = [ x0, ones(1,k).*(2*k) ]';         
+		x0_df = ones(1,k).*(2*k);   
+    elseif strcmp( dist, 'iRiesz2' )
+		x0_df = ones(1,k).*(2*k);   
     elseif strcmp( dist, 'tRiesz' )
-		x0 = [ x0, ones(1,k).*(2*k), 5 ]'; 
+		x0_df = [ ones(1,k).*(2*k), 5 ]; 
+    elseif strcmp( dist, 'tRiesz2' )
+		x0_df = [ ones(1,k).*(2*k), 5 ]; 
     elseif strcmp( dist, 'itRiesz' )
-		x0 = [ x0, ones(1,k).*(2*k), 5 ]';    
+		x0_df = [ ones(1,k).*(2*k), 5 ];
+    elseif strcmp( dist, 'itRiesz2' )
+		x0_df = [ ones(1,k).*(2*k), 5 ];
     elseif strcmp( dist, 'FRiesz' )
-		x0 = [ x0, ones(1,k).*(2*k), ones(1,k).*(2*k) ]';        
+		x0_df = [ ones(1,k).*(2*k+3), ones(1,k).*(2*k+3) ];
+    elseif strcmp( dist, 'FRiesz2' )
+		x0_df = [ ones(1,k).*(2*k+3), ones(1,k).*(2*k+3) ]; 
+    end
+    % Candidate Starting Points for Optimization:
+    x0_1 = [ones(1,p)*.1/p, ones(1,q)*.75/q, x0_df]';
+    x0_2 = [ones(1,p)*.01/p, ones(1,q)*.75/q, x0_df]';
+    x0_3 = [zeros(1,p), ones(1,q)*.75/q, x0_df]';
+    x0_4 = [zeros(1,p+q), x0_df]';    
+    if ~isinf(obj_fun(x0_1)) && ~isnan(obj_fun(x0_1))
+        x0 = x0_1;
+    elseif ~isinf(obj_fun(x0_2)) && ~isnan(obj_fun(x0_2))
+        x0 = x0_2;
+    elseif ~isinf(obj_fun(x0_3)) && ~isnan(obj_fun(x0_3))
+        x0 = x0_3;
+    elseif ~isinf(obj_fun(x0_4)) && ~isnan(obj_fun(x0_4))
+        x0 = x0_4;
     end
 end
 % Restrictions-------------------------------------------------------------
@@ -58,21 +81,32 @@ elseif strcmp( dist, 'itWish' )
     lb = [-inf(p+q,1); k+1; 0];    
 elseif strcmp( dist, 'Riesz' )
     lb = [-inf(p+q,1)', 0:k-1]';  
+elseif strcmp( dist, 'Riesz2' )
+    lb = [-inf(p+q,1)', fliplr(0:k-1)]'; %see stochstic representation
 elseif strcmp( dist, 'iRiesz' )
-    lb = [-inf(p+q,1)', 2:k+1]';  
+    lb = [-inf(p+q,1)', 2:k+1]';
+elseif strcmp( dist, 'iRiesz2' )
+    lb = [-inf(p+q,1)', fliplr(2:k+1)]'; %conjectured
 elseif strcmp( dist, 'tRiesz' )
     lb = [-inf(p+q,1)', 0:k-1, 2]'; 
+elseif strcmp( dist, 'tRiesz2' )
+    lb = [-inf(p+q,1)', fliplr(0:k-1), 2]'; %see stochstic representation
 elseif strcmp( dist, 'itRiesz' )
-    lb = [-inf(p+q,1)', 0:k-1, 2]';
+    lb = [-inf(p+q,1)', 2:k+1, 2]'; %conjectured
+elseif strcmp( dist, 'itRiesz2' )
+    lb = [-inf(p+q,1)', fliplr(2:k+1), 2]'; %conjectured
 elseif strcmp( dist, 'FRiesz' )
-    lb = [-inf(p+q,1)', (0:k-1), (k-(1:k)+2)]';    
+    lb = [-inf(p+q,1)', (0:k-1), (k-(1:k)+2)]';
+elseif strcmp( dist, 'FRiesz2' )
+    lb = [-inf(p+q,1)', (k-(1:k)+2), (0:k-1) ]'; %conjectured
 end
-A = [ ones(1,p+q) zeros(1, length(x0)-p-q) ];   % Stationarity
-b = 1;                                          % Stationarity
+% A = [ ones(1,p+q) zeros(1, length(x0)-p-q) ];   % Stationarity
+% b = 1;                                          % Stationarity
 % Optimization-------------------------------------------------------------
 disp('-------------------------------------------------------------------')
 disp('-------------------------------------------------------------------')
-disp(strcat('Starting caicov scalar BEKK ',num2str(p),' ',num2str(q),' ',dist,' estimation, targeting.'))
+disp(strcat("Optimization Starting Point for Recursion Parameters: p = (", num2str(x0(1:p)'), "), q = (", num2str(x0(p+1:p+q)), ")."))
+disp(strcat('Starting caicov scalar BEKK(',num2str(p),',',num2str(q),')-',dist,' estimation, targeting.'))
 disp('-------------------------------------------------------------------')
 disp('-------------------------------------------------------------------')
 [eparam,optimoutput] = ...
@@ -86,12 +120,12 @@ disp('-------------------------------------------------------------------')
  	);
 % Implementing an algo for permuting order of assets akin to Blasques et
 % al.
-if optimize_ordering
+if optimize_ordering && contains(dist,'Riesz')
     
     perm_improvement = inf;
     while perm_improvement > 0
         % Try Loglike values for 100 random asset permutations, find Min.  
-        rng(1)
+        rng(1) % Always try the same permutations
         for ii = 1:100
             perm_(ii,:) = randperm(k);
         end
@@ -127,7 +161,7 @@ if ~isfield(optimoutput{end},'perm_')
 end
 disp('-------------------------------------------------------------------')
 disp('-------------------------------------------------------------------')
-disp(strcat('Finished caicov scalar BEKK ',num2str(p),' ',num2str(q),' ',dist,' estimation, targeting.'))
+disp(strcat('Finished caicov scalar BEKK(',num2str(p),',',num2str(q),')-',dist,' estimation, targeting.'))
 disp('-------------------------------------------------------------------')
 disp('-------------------------------------------------------------------')
 % [eparam,optimoutput] = ...
@@ -180,8 +214,82 @@ function [ nLogL, logLcontr, Sigma_, param, fitplot ] = obj_fun_wrapper(param, X
         nLogL = inf;
         return
     end
-    
-    vechcholSig = vechchol( mean(X,3)*(1-sum(param(1:p+q))) );
+
+    meanX = mean(X,3);
+    k = size(X,1);
+    if strcmp( dist, 'Wish' )
+        n = param(p+q+1);
+        meanSig = meanX/n;
+    elseif strcmp( dist, 'iWish' )
+        n = param(p+q+1);
+        meanSig = meanX*(n-k-1); 
+    elseif strcmp( dist, 'tWish' )
+        n = param(p+q+1);
+        nu = param(p+q+2);
+        meanSig = meanX/n/nu*(nu-2);   
+    elseif strcmp( dist, 'itWish' )
+        n = param(p+q+1);
+        meanSig = meanX*(n-k-1); 
+    elseif strcmp( dist, 'F' )
+        n = param(p+q+1);
+        nu = param(p+q+2);
+        meanSig = meanX/n/nu*(n-k-1);    
+    elseif strcmp( dist, 'Riesz' )
+        n = param(p+q+1:p+q+k);
+        cholmeanX = chol(meanX,'lower');
+        cholmeanSig = cholmeanX/sqrtm(diag(n));
+        meanSig = cholmeanSig*cholmeanSig';  
+    elseif strcmp( dist, 'Riesz2' )
+        n = param(p+q+1:p+q+k);
+        cholmeanX = cholU(meanX);
+        cholmeanSig = cholmeanX/sqrtm(diag(n));
+        meanSig = cholmeanSig*cholmeanSig';        
+    elseif strcmp( dist, 'iRiesz' )
+        n = param(p+q+1:p+q+k);
+        cholmeanX = cholU(meanX);
+        cholmeanSig = cholmeanX/sqrtm(matviRieszexpmat(n));
+        meanSig = cholmeanSig*cholmeanSig';   
+    elseif strcmp( dist, 'iRiesz2' )
+        n = param(p+q+1:p+q+k);
+        cholmeanX = chol(meanX,'lower');
+        cholmeanSig = cholmeanX/sqrtm(matviRiesz2expmat(n));
+        meanSig = cholmeanSig*cholmeanSig';   
+    elseif strcmp( dist, 'tRiesz' )
+        n = param(p+q+1:p+q+k);
+        nu = param(p+q+k+1);
+        cholmeanX = chol(meanX,'lower');
+        cholmeanSig = cholmeanX/sqrtm(diag(n)*nu/(nu-2));
+        meanSig = cholmeanSig*cholmeanSig';
+    elseif strcmp( dist, 'tRiesz2' )
+        n = param(p+q+1:p+q+k);
+        nu = param(p+q+k+1);
+        cholmeanX = cholU(meanX);
+        cholmeanSig = cholmeanX/sqrtm(diag(n)*nu/(nu-2));
+        meanSig = cholmeanSig*cholmeanSig';   
+    elseif strcmp( dist, 'itRiesz' )
+        n = param(p+q+1:p+q+k);
+        cholmeanX = cholU(meanX);
+        cholmeanSig = cholmeanX/sqrtm(matviRieszexpmat(n));
+        meanSig = cholmeanSig*cholmeanSig';   
+    elseif strcmp( dist, 'itRiesz2' )
+        n = param(p+q+1:p+q+k);
+        cholmeanX = chol(meanX,'lower');
+        cholmeanSig = cholmeanX/sqrtm(matviRiesz2expmat(n));
+        meanSig = cholmeanSig*cholmeanSig';
+    elseif strcmp( dist, 'FRiesz' )
+        n = param(p+q+1:p+q+k);
+        nu = param(p+q+k+1:p+q+k+k);
+        cholmeanX = chol(meanX,'lower');
+        cholmeanSig = cholmeanX/sqrtm(matvFRieszexpmat(n,nu));
+        meanSig = cholmeanSig*cholmeanSig';
+    elseif strcmp( dist, 'FRiesz2' )
+        n = param(p+q+1:p+q+k);
+        nu = param(p+q+k+1:p+q+k+k);
+        cholmeanX = cholU(meanX);
+        cholmeanSig = cholmeanX/sqrtm(matvFRiesz2expmat(n,nu));
+        meanSig = cholmeanSig*cholmeanSig';
+    end
+    vechcholSig = vechchol( meanSig*(1-sum(param(1:p+q))) );
     
     [ nLogL, logLcontr, Sigma_, param, fitplot ] = caicov_scalar_BEKK_likeRec( ...
         [vechcholSig; param], ...
--- toolboxes_own/caicov/caicov_scalar_BEKK_likeRec.m
+++ toolboxes_own/caicov/caicov_scalar_BEKK_likeRec.m
@@ -1,4 +1,4 @@
-function [ nLogL, logLcontr, SigmaE, varargout ] = ...
+function [ nLogL, logLcontr, Sigma_, varargout ] = ...
     caicov_scalar_BEKK_likeRec( param, p, q, X, dist )
 %
 % Michael Stollenwerk
@@ -8,36 +8,124 @@ function [ nLogL, logLcontr, SigmaE, varargout ] = ...
 [k,~,T] = size(X);
 k_ = k*(k+1)/2;
 %% Parameters
+% Initialize with Backcast. Code copied from Sheppard MFE Toolbox.
+m = ceil(sqrt(T));
+w = .06 * .94.^(0:(m-1));
+w = reshape(w/sum(w),[1 1 m]);
+backCast = sum(bsxfun(@times,w,X(:,:,1:m)),3);
+% % Initialize recursion at unconditional mean (stationarity assumed).
+% ini = intrcpt./(1 - sum(garchparam) - sum(archparam));
+
 intrcpt = ivechchol(param(1:k_));
 archparam = param(k_ + 1 : k_ + p);
 garchparam = param(k_ + p + 1 : k_+ p + q);
 
 if strcmp( dist, 'Wish' )
-    n = param(k_+ p + q + 1);    
+    n = param(k_+ p + q + 1);
+    ini = backCast/n;
+    loglike = @(x1,x2,x3) matvWishlike(x1,x2,x3);
 elseif strcmp( dist, 'iWish' )
-    n = param(k_+ p + q + 1);  
+    n = param(k_+ p + q + 1);
+    ini = backCast*(n-k-1); 
+    loglike = @(x1,x2,x3) matviWishlike(x1,x2,x3);   
 elseif strcmp( dist, 'tWish' )
     n = param(k_+ p + q + 1); 
     nu = param(k_+ p + q + 2);
+    ini = backCast/n/nu*(nu-2);  
+    loglike = @(x1,x2,x3,x4) matvtWishlike(x1,x2,x3,x4);    
 elseif strcmp( dist, 'itWish' )
     n = param(k_+ p + q + 1); 
     nu = param(k_+ p + q + 2);
+    ini = backCast*(n-k-1); 
+    loglike = @(x1,x2,x3,x4) matvitWishlike(x1,x2,x3,x4);
 elseif strcmp( dist, 'F' )
     n = param(k_+ p + q + 1); 
     nu = param(k_+ p + q + 2);
+    ini = backCast/n/nu*(n-k-1);
+    loglike = @(x1,x2,x3,x4) matvFlike(x1,x2,x3,x4);    
 elseif strcmp( dist, 'Riesz' )
-    n = param(k_+ p + q + 1 : k_+ p + q + k);    
+    n = param(k_+ p + q + 1 : k_+ p + q + k);
+    ini = zeros(k);
+    for ii = 1:m
+        CCC = chol(X(:,:,ii),'lower')/sqrtm(diag(n));
+        ini = ini + w(ii)*(CCC*CCC');
+    end
+    loglike = @(x1,x2,x3) matvRieszlike(x1,x2,x3);    
+elseif strcmp( dist, 'Riesz2' )
+    n = param(k_+ p + q + 1 : k_+ p + q + k);
+    for ii = 1:m
+        CCC = cholU(X(:,:,ii),'lower')/sqrtm(diag(n));
+        ini = ini + w(ii)*(CCC*CCC');
+    end
+    loglike = @(x1,x2,x3) matvRiesz2like(x1,x2,x3);        
 elseif strcmp( dist, 'iRiesz' )
     n = param(k_+ p + q + 1 : k_+ p + q + k);  
+    ini = zeros(k);
+    for ii = 1:m
+        CCC = chol(X(:,:,ii),'lower')/sqrtm(matviRieszexpmat(n));
+        ini = ini + w(ii)*(CCC*CCC');
+    end
+    loglike = @(x1,x2,x3) matviRieszlike(x1,x2,x3); 
+elseif strcmp( dist, 'iRiesz2' )
+    n = param(k_+ p + q + 1 : k_+ p + q + k); 
+    ini = zeros(k);
+    for ii = 1:m
+        CCC = chol(X(:,:,ii),'lower')/sqrtm(matviRiesz2expmat(n));
+        ini = ini + w(ii)*(CCC*CCC');
+    end
+    loglike = @(x1,x2,x3) matviRiesz2like(x1,x2,x3); 
 elseif strcmp( dist, 'tRiesz' )
     n = param(k_+ p + q + 1 : k_+ p + q + k); 
     nu = param(k_+ p + q + k + 1);
+    ini = zeros(k);
+    for ii = 1:m
+        CCC = chol(X(:,:,ii),'lower')/sqrtm(diag(n)*nu/(nu-2));
+        ini = ini + w(ii)*(CCC*CCC');
+    end
+    loglike = @(x1,x2,x3,x4) matvtRieszlike(x1,x2,x3,x4); 
+elseif strcmp( dist, 'tRiesz2' )
+    n = param(k_+ p + q + 1 : k_+ p + q + k); 
+    nu = param(k_+ p + q + k + 1);
+    ini = zeros(k);
+    for ii = 1:m
+        CCC = cholU(X(:,:,ii),'lower')/sqrtm(diag(n)*nu/(nu-2));
+        ini = ini + w(ii)*(CCC*CCC');
+    end    
+    loglike = @(x1,x2,x3,x4) matvtRiesz2like(x1,x2,x3,x4); 
 elseif strcmp( dist, 'itRiesz' )
     n = param(k_+ p + q + 1 : k_+ p + q + k); 
     nu = param(k_+ p + q + k + 1);
+    ini = zeros(k);
+    for ii = 1:m
+        CCC = cholU(X(:,:,ii),'lower')/sqrtm(matviRieszexpmat(n));
+        ini = ini + w(ii)*(CCC*CCC');
+    end   
+    loglike = @(x1,x2,x3,x4) matvitRieszlike(x1,x2,x3,x4); 
+elseif strcmp( dist, 'itRiesz2' )
+    n = param(k_+ p + q + 1 : k_+ p + q + k); 
+    nu = param(k_+ p + q + k + 1);
+    ini = zeros(k);
+    for ii = 1:m
+        CCC = chol(X(:,:,ii),'lower')/sqrtm(matviRiesz2expmat(n));
+        ini = ini + w(ii)*(CCC*CCC');
+    end
+    loglike = @(x1,x2,x3,x4) matvitRiesz2like(x1,x2,x3,x4); 
 elseif strcmp( dist, 'FRiesz' )
     n = param(k_+ p + q + 1 : k_+ p + q + k); 
-    nu = param(k_+ p + q + k + 1 : k_+ p + q + k + k);        
+    nu = param(k_+ p + q + k + 1 : k_+ p + q + k + k); 
+    for ii = 1:m
+        CCC = chol(X(:,:,ii),'lower')/sqrtm(matvFRieszexpmat(n,nu));
+        ini = ini + w(ii)*(CCC*CCC');
+    end
+    loglike = @(x1,x2,x3,x4) matvFRieszlike(x1,x2,x3,x4); 
+elseif strcmp( dist, 'FRiesz2' )
+    n = param(k_+ p + q + 1 : k_+ p + q + k); 
+    nu = param(k_+ p + q + k + 1 : k_+ p + q + k + k);  
+    for ii = 1:m
+        CCC = cholU(X(:,:,ii),'lower')/sqrtm(matvFRiesz2expmat(n,nu));
+        ini = ini + w(ii)*(CCC*CCC');
+    end
+    loglike = @(x1,x2,x3,x4) matvFRiesz2like(x1,x2,x3,x4); 
 end
 
 if nargout >= 4
@@ -55,79 +143,44 @@ if nargout >= 4
     varargout{1} = param_out;
 end
 %% Data Storage
-SigmaE = NaN(k,k,T+1);
+Sigma_ = NaN(k,k,T+22);
 logLcontr = NaN(T,1);
 %% Recursion
-% Initialize with Backcast. Code copied from Sheppard MFE Toolbox.
-m = ceil(sqrt(T));
-w = .06 * .94.^(0:(m-1));
-w = reshape(w/sum(w),[1 1 m]);
-backCast = sum(bsxfun(@times,w,X(:,:,1:m)),3);
-ini = backCast;
-% % Initialize recursion at unconditional mean (stationarity assumed).
-% ini = intrcpt./(1 - sum(garchparam) - sum(archparam));
+
 for tt=1:T
-    SigmaE(:,:,tt) = intrcpt;
+    
+    Sigma_(:,:,tt) = intrcpt;
     for jj = 1:p
         if (tt-jj) <= 0
-            SigmaE(:,:,tt) = SigmaE(:,:,tt) + archparam(jj)*ini;
+            Sigma_(:,:,tt) = Sigma_(:,:,tt) + archparam(jj)*ini;
         else
-            SigmaE(:,:,tt) = SigmaE(:,:,tt) + archparam(jj)*X(:,:,tt-jj);
+            Sigma_(:,:,tt) = Sigma_(:,:,tt) + archparam(jj)*X(:,:,tt-jj);
         end
     end
     for jj = 1:q
         if (tt-jj) <= 0
-            SigmaE(:,:,tt) = SigmaE(:,:,tt) + garchparam(jj)*ini;
+            Sigma_(:,:,tt) = Sigma_(:,:,tt) + garchparam(jj)*ini;
         else
-            SigmaE(:,:,tt) = SigmaE(:,:,tt) + garchparam(jj)*SigmaE(:,:,tt-jj);
+            Sigma_(:,:,tt) = Sigma_(:,:,tt) + garchparam(jj)*Sigma_(:,:,tt-jj);
         end
     end
 
     try
-        % Centering distributions
-        if strcmp( dist, 'Wish' )
-            Sigma_ = SigmaE(:,:,tt)/n;
-        elseif strcmp( dist, 'iWish' )
-            Sigma_ = SigmaE(:,:,tt)*(n-k-1);
-        elseif strcmp( dist, 'tWish')
-            Sigma_ = SigmaE(:,:,tt)*(nu-2)/nu/n;
-        elseif strcmp( dist, 'itWish')
-            Sigma_ = SigmaE(:,:,tt)*(n-k-1);
-        elseif strcmp( dist, 'F' )
-            Sigma_ = SigmaE(:,:,tt)*(nu-k-1)/n/nu;
-        elseif strcmp( dist, 'Riesz' )
-            cholSigE = chol(SigmaE(:,:,tt),'lower');
-            Sigma_ = cholSigE/diag(n)*cholSigE';
-        elseif strcmp( dist, 'iRiesz' )
-            invCholInvSigE = inv(chol(inv(SigmaE(:,:,tt)),'lower'));
-            M = matviRieszexpmat(n);
-            Sigma_ = invCholInvSigE'/M*invCholInvSigE;
-        elseif strcmp( dist, 'tRiesz' )
-            cholSigE = chol(SigmaE(:,:,tt),'lower');
-            Sigma_ = cholSigE/diag(n)*cholSigE'.*(nu - 2)./nu;
-        elseif strcmp( dist, 'itRiesz' )
-            invCholInvSigE = inv(chol(inv(SigmaE(:,:,tt)),'lower'));
-            M = matviRieszexpmat(n);
-            Sigma_ = invCholInvSigE'/M*invCholInvSigE;
-        elseif strcmp( dist, 'FRiesz' )
-            cholSigE = chol(SigmaE(:,:,tt),'lower');
-            M = matvFRieszexpmat(n,nu);
-            Sigma_ = cholSigE/M*cholSigE';
-        end
         % Likelihood Evaluation    
         if exist('nu','var')
-            logLcontr(tt) = logpdf( dist, X(:,:,tt), Sigma_, n, nu );
+            [nLogLcontr, ~, score, ~, ~, fisherinfo] = loglike( Sigma_(:,:,tt), n, nu, X(:,:,tt) );
         else
-            logLcontr(tt) = logpdf( dist, X(:,:,tt), Sigma_, n );
+            [nLogLcontr, ~, score, ~, ~, fisherinfo] = loglike( Sigma_(:,:,tt), n, X(:,:,tt) );
         end
+    logLcontr(tt) = -nLogLcontr;
     catch ME
+%         tt
 %         ME.message
 %         [ME.stack.line]'
 %         {ME.stack.name}'
         nLogL = inf;
         logLcontr = NaN;
-        SigmaE = NaN;
-        varargout{1} = NaN;   
+        varargout{1} = NaN;      
         varargout{2} = NaN;
         return
     end
@@ -135,16 +188,16 @@ for tt=1:T
 end
 %% Fcst
 for tt=T+1:T+22
-    SigmaE(:,:,tt) = intrcpt;
+    Sigma_(:,:,tt) = intrcpt;
     for jj = 1:p
         if (tt-jj) > T
-            SigmaE(:,:,tt) = SigmaE(:,:,tt) + archparam(jj)*SigmaE(:,:,tt-jj);
+            Sigma_(:,:,tt) = Sigma_(:,:,tt) + archparam(jj)*Sigma_(:,:,tt-jj);
         else
-            SigmaE(:,:,tt) = SigmaE(:,:,tt) + archparam(jj)*X(:,:,tt-jj);
+            Sigma_(:,:,tt) = Sigma_(:,:,tt) + archparam(jj)*X(:,:,tt-jj);
         end
     end
     for jj = 1:q
-        SigmaE(:,:,tt) = SigmaE(:,:,tt) + garchparam(jj)*SigmaE(:,:,tt-jj);
+        Sigma_(:,:,tt) = Sigma_(:,:,tt) + garchparam(jj)*Sigma_(:,:,tt-jj);
     end
 end
 %% Log-Likelihood
