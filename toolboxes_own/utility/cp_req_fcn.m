b9910aa3501eef2d7eb97d4b3a986d71d8309d8c
mstollenwerk <michael.stollenwerk@live.com>
2021-09-15 16:24:24 +0200

    Update cp_req_fcn.m

--- toolboxes_own/utility/cp_req_fcn.m
+++ toolboxes_own/utility/cp_req_fcn.m
@@ -1,4 +1,4 @@
-function cp_req_fcn( parentFcn )
+function cp_req_fcn( parentpath, destpath )
 %CP_REQ_FCN creates a folder with all user-made functions that parentFcn
 %depends on. Location: desktop
 %
@@ -6,7 +6,7 @@ function cp_req_fcn( parentFcn )
 %  cp_req_fcn('fcn')
 %
 % INPUTS:
-%   PARENT_FCN   - parent function
+%   PARENT_FCN - parent function
 %
 % COMMENTS:
 %
@@ -17,13 +17,14 @@ function cp_req_fcn( parentFcn )
 % michael.stollenwerk@live.com
 % 08.08.2017
 % 05.09.2017: changed description
+% 10.09.2021: added destination folder input
 
-parentpath = which(parentFcn);
-FileList = matlab.codetools.requiredFilesAndProducts(parentFcn);
-basedir = winqueryreg('HKEY_CURRENT_USER', 'Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders', 'Desktop');
-newfolder = [basedir '\reqFcns'];
-mkdir(newfolder)
-copyfile(parentpath,newfolder)
+% parentpath = which(parentFcn);
+FileList = matlab.codetools.requiredFilesAndProducts(parentpath);
+% basedir = winqueryreg('HKEY_CURRENT_USER', 'Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders', 'Desktop');
+% dest_fldr = [basedir '\reqFcns'];
+mkdir(destpath)
+copyfile(parentpath,destpath)
 FileList = arrayfun(@(x) x,FileList);
-cellfun(@(x) copyfile(x,newfolder),FileList);
+cellfun(@(x) copyfile(x,destpath),FileList);
 end
