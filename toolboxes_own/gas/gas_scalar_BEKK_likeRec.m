a9eb639056202a379c27a7928d806823e31d04e7
mstollenwerk <michael.stollenwerk@live.com>
2021-10-06 11:27:52 +0200

    Update gas_scalar_BEKK_likeRec.m
    bugfix in iRiesz recursion initialization

--- toolboxes_own/gas/gas_scalar_BEKK_likeRec.m
+++ toolboxes_own/gas/gas_scalar_BEKK_likeRec.m
@@ -63,7 +63,7 @@ elseif strcmp( dist, 'iRiesz' )
     n = param(k_+ p + q + 1 : k_+ p + q + k);  
     ini = zeros(k);
     for ii = 1:m
-        CCC = chol(X(:,:,ii),'lower')/sqrtm(matviRieszexpmat(n));
+        CCC = cholU(X(:,:,ii))/sqrtm(matviRieszexpmat(n));
         ini = ini + w(ii)*(CCC*CCC');
     end
     loglike = @(x1,x2,x3) matviRieszlike(x1,x2,x3); 
