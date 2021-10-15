50b1820b28ac3698c407ee071d91e5a7ab1fb98a
mstollenwerk <michael.stollenwerk@live.com>
2021-10-15 15:00:07 +0200

    Update matvStandardize.m
    just some renaming of variables

--- toolboxes_own/distributions/matrix_variate_distributions/matvStandardize.m
+++ toolboxes_own/distributions/matrix_variate_distributions/matvStandardize.m
@@ -1,8 +1,8 @@
-function EV = matvStandardize(dist, Sigma_, dfs)
+function Sigma_ = matvStandardize(dist, EVorDta, dfs)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-[k,~,N] = size(Sigma_);
-EV = NaN(k,k,N);
+[k,~,N] = size(EVorDta);
+Sigma_ = NaN(k,k,N);
 
 if strcmp( dist, 'Wish' )
     if length(dfs) ~= 1
@@ -10,7 +10,7 @@ if strcmp( dist, 'Wish' )
     end
     n = dfs(1);
     for ii = 1:N
-        EV(:,:,ii) = Sigma_(:,:,ii)/n;
+        Sigma_(:,:,ii) = EVorDta(:,:,ii)/n;
     end
 elseif strcmp( dist, 'iWish' )
     if length(dfs) ~= 1
@@ -18,7 +18,7 @@ elseif strcmp( dist, 'iWish' )
     end   
     n = dfs(1);
     for ii = 1:N
-        EV(:,:,ii) = Sigma_(:,:,ii)*(n-k-1);
+        Sigma_(:,:,ii) = EVorDta(:,:,ii)*(n-k-1);
     end    
 elseif strcmp( dist, 'tWish' )
     if length(dfs) ~= 2
@@ -27,7 +27,7 @@ elseif strcmp( dist, 'tWish' )
     n = dfs(1); 
     nu = dfs(2);
     for ii = 1:N
-        EV(:,:,ii) = Sigma_(:,:,ii)/n/nu*(nu-2);
+        Sigma_(:,:,ii) = EVorDta(:,:,ii)/n/nu*(nu-2);
     end
 elseif strcmp( dist, 'itWish' )
     if length(dfs) ~= 2
@@ -36,7 +36,7 @@ elseif strcmp( dist, 'itWish' )
     n = dfs(1); 
     nu = dfs(2);
     for ii = 1:N
-        EV(:,:,ii) = Sigma_(:,:,ii)*(n-k-1);
+        Sigma_(:,:,ii) = EVorDta(:,:,ii)*(n-k-1);
     end
 elseif strcmp( dist, 'F' )
     if length(dfs) ~= 2
@@ -45,7 +45,7 @@ elseif strcmp( dist, 'F' )
     n = dfs(1); 
     nu = dfs(2);
     for ii = 1:N
-        EV(:,:,ii) = Sigma_(:,:,ii)/n/nu*(nu-k-1);
+        Sigma_(:,:,ii) = EVorDta(:,:,ii)/n/nu*(nu-k-1);
     end
 elseif strcmp( dist, 'Riesz' )
     if length(dfs) ~= k
@@ -53,8 +53,8 @@ elseif strcmp( dist, 'Riesz' )
     end    
     n = dfs;
     for ii = 1:N
-        L = chol(Sigma_(:,:,ii),'lower');
-        EV(:,:,ii) = L/diag(n)*L';
+        L = chol(EVorDta(:,:,ii),'lower');
+        Sigma_(:,:,ii) = L/diag(n)*L';
     end
 elseif strcmp( dist, 'Riesz2' )
     if length(dfs) ~= k
@@ -62,8 +62,8 @@ elseif strcmp( dist, 'Riesz2' )
     end    
     n = dfs;
     for ii = 1:N
-        U = cholU(Sigma_(:,:,ii));        
-        EV(:,:,ii) = U/diag(n)*U';
+        U = cholU(EVorDta(:,:,ii));        
+        Sigma_(:,:,ii) = U/diag(n)*U';
     end
 elseif strcmp( dist, 'iRiesz' )
     if length(dfs) ~= k
@@ -71,8 +71,8 @@ elseif strcmp( dist, 'iRiesz' )
     end    
     n = dfs;  
     for ii = 1:N
-        U = cholU(Sigma_(:,:,ii));
-        EV(:,:,ii) = U/matviRieszexpmat(n)*U';
+        U = cholU(EVorDta(:,:,ii));
+        Sigma_(:,:,ii) = U/matviRieszexpmat(n)*U';
     end
 elseif strcmp( dist, 'iRiesz2' )
     if length(dfs) ~= k
@@ -80,8 +80,8 @@ elseif strcmp( dist, 'iRiesz2' )
     end    
     n = dfs; 
     for ii = 1:N
-        L = chol(Sigma_(:,:,ii),'lower');
-        EV(:,:,ii) = L/matviRiesz2expmat(n)*L';
+        L = chol(EVorDta(:,:,ii),'lower');
+        Sigma_(:,:,ii) = L/matviRiesz2expmat(n)*L';
     end
 elseif strcmp( dist, 'tRiesz' )
     if length(dfs) ~= k + 1
@@ -90,8 +90,8 @@ elseif strcmp( dist, 'tRiesz' )
     n = dfs(1 : k); 
     nu = dfs(k + 1);
     for ii = 1:N
-        L = chol(Sigma_(:,:,ii),'lower');
-        EV(:,:,ii) = L/(diag(n)*nu/(nu-2))*L';
+        L = chol(EVorDta(:,:,ii),'lower');
+        Sigma_(:,:,ii) = L/(diag(n)*nu/(nu-2))*L';
     end
 elseif strcmp( dist, 'tRiesz2' )
     if length(dfs) ~= k + 1
@@ -100,8 +100,8 @@ elseif strcmp( dist, 'tRiesz2' )
     n = dfs(1 : k); 
     nu = dfs(k + 1);
     for ii = 1:N
-        U = cholU(Sigma_(:,:,ii)); 
-        EV(:,:,ii) = U/(diag(n)*nu/(nu-2))*U';
+        U = cholU(EVorDta(:,:,ii)); 
+        Sigma_(:,:,ii) = U/(diag(n)*nu/(nu-2))*U';
     end
 elseif strcmp( dist, 'itRiesz' )
     if length(dfs) ~= k + 1
@@ -110,8 +110,8 @@ elseif strcmp( dist, 'itRiesz' )
     n = dfs(1 : k); 
     nu = dfs(k + 1);
     for ii = 1:N
-        U = cholU(Sigma_(:,:,ii));
-        EV(:,:,ii) = U/matviRieszexpmat(n)*U';
+        U = cholU(EVorDta(:,:,ii));
+        Sigma_(:,:,ii) = U/matviRieszexpmat(n)*U';
     end
 elseif strcmp( dist, 'itRiesz2' )
     if length(dfs) ~= k + 1
@@ -120,8 +120,8 @@ elseif strcmp( dist, 'itRiesz2' )
     n = dfs(1 : k); 
     nu = dfs(k + 1);
     for ii = 1:N
-        L = chol(Sigma_(:,:,ii),'lower');
-        EV(:,:,ii) = L/matviRiesz2expmat(n)*L';
+        L = chol(EVorDta(:,:,ii),'lower');
+        Sigma_(:,:,ii) = L/matviRiesz2expmat(n)*L';
     end
 elseif strcmp( dist, 'FRiesz' )
     if length(dfs) ~= 2*k
@@ -130,8 +130,8 @@ elseif strcmp( dist, 'FRiesz' )
     n = dfs(1 : k); 
     nu = dfs(k + 1 : k + k); 
     for ii = 1:N
-        L = chol(Sigma_(:,:,ii),'lower');
-        EV(:,:,ii) = L/matvFRieszexpmat(n,nu)*L';
+        L = chol(EVorDta(:,:,ii),'lower');
+        Sigma_(:,:,ii) = L/matvFRieszexpmat(n,nu)*L';
     end
 elseif strcmp( dist, 'FRiesz2' )
     if length(dfs) ~= 2*k
@@ -140,8 +140,8 @@ elseif strcmp( dist, 'FRiesz2' )
     n = dfs(1 : k); 
     nu = dfs(k + 1 : k + k); 
     for ii = 1:N
-        U = cholU(Sigma_(:,:,ii)); 
-        EV(:,:,ii) = U/matvFRies2zexpmat(n,nu)*U';
+        U = cholU(EVorDta(:,:,ii)); 
+        Sigma_(:,:,ii) = U/matvFRies2zexpmat(n,nu)*U';
     end
 end
 
