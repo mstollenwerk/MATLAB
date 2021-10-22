0b146d91685494352f06861f1250fd759fa8b6fd
mstollenwerk <michael.stollenwerk@live.com>
2021-10-22 12:51:44 +0200

    Update matvFRieszlike.m
    - changed fisherinfo approximation to matrix-F approximation

--- toolboxes_own/distributions/matrix_variate_distributions/matvFRieszlike.m
+++ toolboxes_own/distributions/matrix_variate_distributions/matvFRieszlike.m
@@ -114,29 +114,73 @@ end
 %% Hessian (Optional Output)
 %% Fisher Info (Optional Output)
 if nargout >= 6
+   
+    %%% I don't have the exact closed form yet, there are some possible
+    %%% approximation candidates.
     
-    %%% THIS IS ONLY AN APPROXIMATION TO THE FISHER INFO WRT SIGMA!!! %%%
+    % 1. Approximate with matrix-F fisherinfo, replacing n and nu with the
+    % mean(n) and mean(nu). Remember that the FRiesz becomes the matrix-F
+    % if all n are equal and all nu are equal. This seems the fairest
+    % approximation.
+    n_ = mean(n);
+    nu_ = mean(nu);    
+    c_1 = (n_^2*(nu_-p-2) + 2*n_)/((nu_-p)*(nu_-p-1)*(nu_-p-3));
+    c_2 = (n_*(nu_-p-2)+n_^2+n_)/((nu_-p)*(nu_-p-1)*(nu_-p-3));
+    c_4 = (n_-p-1)/((n_+nu_-1)*(n_+nu_+2))*((n_-p-2+1/(nu_+n_))*c_2-(1+(n_-p-1)/(n_+nu_))*c_1);
+    c_3 = (n_-p-1)/(n_+nu_)*((n_-p-2)*c_2 - c_1)-(n_+nu_+1)*c_4;
+    invSigF = inv(Sigma_);
+    
+    G = Dmatrix(p);
+    conskron2 = (nu_-(n_+nu_)*(c_3+c_4));
+    consvec2 = (n_+nu_)*c_4;
+    fishSig = 1/2*G'*(conskron2*kron2(invSigF) - consvec2*vec2(invSigF))*G;
+    
+     
+%     invSig = inv(Sigma_);
+%     [G, iG] = Dmatrix(p);
+%     L = ELmatrix(p);
+%     I = speye(p);
+%     iC = inv(C);
+
+      % 2. Approximate with part Riesz, part matrix-F fisherinfo. This is
+      % motivated by the fact that there are two summands in the FRiesz
+      % fisherinfo, the first one we have derived, it is equal to the Riesz
+      % fisherinfo, the second one we have not derived its expectation yet.
+      % The matrix-F has the same structure, but for the matrix-F we have
+      % the expectation of the second summand as well. So we glue the first
+      % summand of the FRiezs fisherinfo with the second summand of the
+      % matrix-F fisherinfo. This seems a bit aribtrary and does not work
+      % well in practice (measured by the fit).
 %     n_ = mean(n);
-%     nu_ = mean(nu);    
+%     nu_ = mean(nu); 
 %     c_1 = (n_^2*(nu_-p-2) + 2*n_)/((nu_-p)*(nu_-p-1)*(nu_-p-3));
 %     c_2 = (n_*(nu_-p-2)+n_^2+n_)/((nu_-p)*(nu_-p-1)*(nu_-p-3));
 %     c_4 = (n_-p-1)/((n_+nu_-1)*(n_+nu_+2))*((n_-p-2+1/(nu_+n_))*c_2-(1+(n_-p-1)/(n_+nu_))*c_1);
 %     c_3 = (n_-p-1)/(n_+nu_)*((n_-p-2)*c_2 - c_1)-(n_+nu_+1)*c_4;
-%     invSigF = inv(Sigma_);
+% 
+%     term_Riesz = 1/2*G'*kron(C'\diag(n),invSig)*L'/(iG*kron(C,I)*L');
+%     term_Rest = -(n_ + nu_)/2*G'*((c_3+c_4)*kron2(invSig) + c_4*vec2(invSig))*G;
 %     
-%     G = Dmatrix(p);
-%     ckron2 = (nu_-(n_+nu_)*(c_3+c_4));
-%     cvec2 = (n_+nu_)*c_4;
-%     fisherinfo.Sigma_ = 1/2*G'*(ckron2*kron2(invSigF) - cvec2*vec2(invSigF))*G;
-    G = Dmatrix(p);
-    L = ELmatrix(p);
-    I = speye(p);
+%     fishSig = term_Riesz + term_Rest;
     
-    iC = inv(C);
+      % 3. Approximate by replacing the expectation of the second summand
+      % of the hessian (See 2. Approximation for explanation) hessian,
+      % where A is replaced by expectation of A ( so basically do f(E[A]) 
+      % instead of E[f(A)] ). This provides good fit, but gives completely
+      % out of the range paramter values for the dynamic parameters (~ -3
+      % for the score parameter), which are not comparable to other GAS
+      % models
+%     fishSig = .5*G'*kron(iC',C'\diag(nu)/C)*L'/(G'*kron(C,I)*L')*(G'*G) ...
+%               - G'*kron(iC',C'\diag(nu + n)/C)*L'/(G'*kron(C,I)*L')*(G'*G);
+          
+%     % 4. Approximate by Riesz fisherinfo since for nu->infinity, the
+%     FRiesz goes to the Riesz.
+%     fishSig = 1/2*G'*kron(C'\diag(n),invSig)*L'/(iG*kron(C,I)*L');
     
-    %%% THIS IS ONLY AN APPROXIMATION TO THE FISHER INFO WRT SIGMA!!! %%%
-    fishSig = .5*G'*kron(iC',C'\diag(nu)/C)*L'/(G'*kron(C,I)*L')*(G'*G) ...
-              - G'*kron(iC',C'\diag(nu + n)/C)*L'/(G'*kron(C,I)*L')*(G'*G);
+%     % 5. Approximate by Wishart fisherinfo since for nu->infinity, all n
+%     equal and all nu equal the FRiezs converges to the Wishart. Replace n
+%     by mean(n).
+%     fishSig = mean(n)/2*G'*kron2(invSig)*G;
     
     fisherinfo.Sigma_ = fishSig;
     fisherinfo.df_1 = NaN;
