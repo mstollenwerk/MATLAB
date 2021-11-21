d7e8def0853b2ef85780eb8c248e86419a3969b2
mstollenwerk <michael.stollenwerk@live.com>
2021-11-21 10:02:15 +0100

    Update makepd.m

--- toolboxes_own/utility/makepd.m
+++ toolboxes_own/utility/makepd.m
@@ -1,10 +1,19 @@
 function PDmatrix = makepd(nonPDmatrix)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-[X,Y] = eig(nonPDmatrix);
-Y(Y<0) = eps^(1/3);
-PDmatrix = X*Y*X';
-PDmatrix = .5*(PDmatrix+PDmatrix');
+N = size(nonPDmatrix,3);
 
+PDmatrix = NaN(size(nonPDmatrix));
+
+for ii = 1:N
+    [X,Y] = eig(nonPDmatrix(:,:,ii));
+    if any(diag(Y) < 0)
+        warning('pd adjustment made')
+    end
+    Y(Y<0) = eps^(1/3);
+    y = X*Y*X';
+    PDmatrix(:,:,ii) = .5*(y+y');
+end
+    
 end
 
