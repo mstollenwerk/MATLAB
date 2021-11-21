4ff6cec871a9b1444e4f79a68f269a716693cd9a
mstollenwerk <michael.stollenwerk@live.com>
2021-11-21 10:08:48 +0100

    Update makepd.m

--- toolboxes_own/utility/makepd.m
+++ toolboxes_own/utility/makepd.m
@@ -1,18 +1,25 @@
-function PDmatrix = makepd(nonPDmatrix)
+function [PDmatrix, varargout] = makepd(nonPDmatrix)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 N = size(nonPDmatrix,3);
 
 PDmatrix = NaN(size(nonPDmatrix));
 
+adjustments_made = zeros(N,1);
 for ii = 1:N
     [X,Y] = eig(nonPDmatrix(:,:,ii));
     if any(diag(Y) < 0)
-        warning('pd adjustment made')
+        Y(Y<0) = eps^(1/3);
+        y = X*Y*X';
+        PDmatrix(:,:,ii) = .5*(y+y');
+        adjustments_made(ii) = sum(diag(Y) < 0);
+    else
+        PDmatrix(:,:,ii) = nonPDmatrix(:,:,ii);
     end
-    Y(Y<0) = eps^(1/3);
-    y = X*Y*X';
-    PDmatrix(:,:,ii) = .5*(y+y');
+end
+
+if nargout == 2
+    varargout{1} = adjustments_made;
 end
     
 end
