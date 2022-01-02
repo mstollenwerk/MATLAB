e72713c2544fac7ab3183d59c9102542ee0ef6e8
mstollenwerk <michael.stollenwerk@live.com>
2022-01-02 18:35:47 +0100

    Update riskmetrics_.m
    backcast instead of mean initialization

--- toolboxes_own/riskmetrics/riskmetrics_.m
+++ toolboxes_own/riskmetrics/riskmetrics_.m
@@ -3,8 +3,13 @@ function [Sigma_, fcst_Sigma] = riskmetrics_( data_RC )
 
 [N,~,T] = size(data_RC);
 
+m = ceil(sqrt(T));
+w = .06 * .94.^(0:(m-1));
+w = reshape(w/sum(w),[1 1 m]);
+backCast = sum(bsxfun(@times,w,data_RC(:,:,1:m)),3);
+
 Sigma_ = NaN(N,N,T+1);
-Sigma_(:,:,1) = mean(data_RC,3);
+Sigma_(:,:,1) = backCast;
 for tt = 2:T+1
     Sigma_(:,:,tt) = .06*data_RC(:,:,tt-1) + .94*Sigma_(:,:,tt-1);
 end
