ffe27c52c7477038fa573d3b40636c240039d2da
mstollenwerk <michael.stollenwerk@live.com>
2022-03-15 08:04:33 +0100

    Create vech3d.m

---
+++ toolboxes_own/utility/vech3d.m
@@ -0,0 +1,26 @@
+function vechMatrix = vech3d(Matrix)
+% Half-vectorizes a matrix
+% 
+%% Inputs/Outputs
+%
+% Inputs:
+%   MATRIXDATA   - A K by K by T symmetric matrix array
+%
+% Outputs:
+%   STACKEDDATA  - A T by K(K+1)/2 vector of stacked data
+%
+% Comments:
+%   The data in row t (element of 1:T) is stacked according to 
+%     [ data(1) ...        ...         ...               ...
+%       data(2) data(K+1)  ...         ...               ...
+%       data(3) data(K+2)  data(2K)    ...               ...
+%       ...     ....       ...         ...               ...
+%       data(K) data(2K-1) ...         data(K(K+1)/2-1)  data(K(K+1)/2) ]
+
+%% Input Checking
+[k,~,T] = size(Matrix);
+vechMatrix = NaN(T,k*(k+1)/2);
+for tt = 1:T
+    vechMatrix(tt,:) = vech(Matrix(:,:,tt));
+end
+end
