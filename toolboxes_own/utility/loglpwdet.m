e185b0911d497a5dea451d89c990578316da4bf0
mstollenwerk <michael.stollenwerk@live.com>
2022-03-23 10:22:46 +0100

    Update loglpwdet.m

--- toolboxes_own/utility/loglpwdet.m
+++ toolboxes_own/utility/loglpwdet.m
@@ -5,6 +5,9 @@ if size(powers,1)<size(powers,2)
     powers = powers';
 end
 if nargin >= 3 % Allow direct input of diagL
+    if size(varargin{1}) ~= size(powers)
+        error('Third input must be column vector of diagonal entries of Cholesky decomposition')
+    end
     diagL = varargin{1};
 else
     diagL = diag(chol(X,'lower'));
