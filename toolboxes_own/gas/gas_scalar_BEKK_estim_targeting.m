02049b2a34b5fb4ea4585184bac14939679fa197
mstollenwerk <michael.stollenwerk@live.com>
2022-03-23 10:22:36 +0100

    Update gas_scalar_BEKK_estim_targeting.m
    Only do permutation optimization for Riezs-named distributions

--- toolboxes_own/gas/gas_scalar_BEKK_estim_targeting.m
+++ toolboxes_own/gas/gas_scalar_BEKK_estim_targeting.m
@@ -52,35 +52,45 @@ if isempty(x0)
     x0 = [zeros(1,p+q), x0_df]'; % This x0 should always work, so you can get rid of all the other ones and the try/catch around the optimization.
 %           ones(1,p)*.05/p, ones(1,q)*.98/q, x0_df]';    
 end
-% Restrictions-------------------------------------------------------------
+% Optimization Specs ------------------------------------------------------
 if strcmp( dist, 'Wish' )
     lb = [-0.001; -inf(p+q-1,1); k-1];
+    perm_optim = 0;
 elseif strcmp( dist, 'iWish' )
     lb = [-0.001; -inf(p+q-1,1); k+1];
+    perm_optim = 0;
 elseif strcmp( dist, 'F' )
     lb = [-0.001; -inf(p+q-1,1); k-1; k+1];
+    perm_optim = 0;
 elseif strcmp( dist, 'tWish' )
     lb = [-0.001; -inf(p+q-1,1); k-1; 2];
+    perm_optim = 0;
 elseif strcmp( dist, 'itWish' )
     lb = [-0.001; -inf(p+q-1,1); k+1; 0];    
+    perm_optim = 0;
 elseif strcmp( dist, 'Riesz' )
     lb = [-0.001, -inf(p+q-1,1)', 0:k-1]';  
+    perm_optim = 1;
 % elseif strcmp( dist, 'Riesz2' )
 %     lb = [-0.001, -inf(p+q-1,1)', fliplr(0:k-1)]'; %see stochstic representation
 % elseif strcmp( dist, 'iRiesz' )
 %     lb = [-0.001, -inf(p+q-1,1)', 2:k+1]';
 elseif strcmp( dist, 'iRiesz2' )
     lb = [-0.001, -inf(p+q-1,1)', fliplr(2:k+1)]'; %conjectured
+    perm_optim = 1;
 elseif strcmp( dist, 'tRiesz' )
     lb = [-0.001, -inf(p+q-1,1)', 0:k-1, 2]'; 
+    perm_optim = 1;
 % elseif strcmp( dist, 'tRiesz2' )
 %     lb = [-0.001, -inf(p+q-1,1)', fliplr(0:k-1), 2]'; %see stochstic representation
 % elseif strcmp( dist, 'itRiesz' )
 %     lb = [-0.001, -inf(p+q-1,1)', 2:k+1, 2]'; %conjectured
 elseif strcmp( dist, 'itRiesz2' )
     lb = [-0.001, -inf(p+q-1,1)', fliplr(2:k+1), 2]'; %conjectured
+    perm_optim = 1;
 elseif strcmp( dist, 'FRiesz' )
     lb = [-0.001, -inf(p+q-1,1)', (0:k-1), (k-(1:k)+2)]'; %Note that scoreparam(1) has a high LB. Unfortunatelx FRiesz estimation seems very hard.
+    perm_optim = 1;
 % elseif strcmp( dist, 'FRiesz2' )
 %     lb = [-0.001, -inf(p+q-1,1)', (k-(1:k)), (2:k+1) ]'; %conjectured %Note that scoreparam(1) has a high LB. Unfortunatelx FRiesz estimation seems very hard.
 end
@@ -95,15 +105,29 @@ for ii = 1:size(x0,2) % Looping through candidate starting points, until one wor
         %disp(strcat("Starting gas scalar BEKK(",num2str(p),",",num2str(q),")-",dist," estimation, targeting."))
         %disp('-------------------------------------------------------------------')
         %disp('-------------------------------------------------------------------')
-        [eparam,optimoutput] = ...
-            fmincon_Rieszperm(...
-                k, ...
-                obj_fun, ...
-                x0(:,ii), ...
-                [],[],[],[], ...
-                lb,[],[], ...
-                varargin{:} ...
-            );
+        if perm_optim
+            [eparam,optimoutput] = ...
+                fmincon_Rieszperm(...
+                    k, ...
+                    obj_fun, ...
+                    x0(:,ii), ...
+                    [],[],[],[], ...
+                    lb,[],[], ...
+                    varargin{:} ...
+                );
+            obj_fun_opt_perm = @(param) obj_fun(param,optimoutput.perm_);
+        else
+            [eparam,optimoutput] = ...
+                my_fmincon(...
+                    @(param) obj_fun(param,1:k), ...
+                    x0(:,ii), ...
+                    [],[],[],[], ...
+                    lb,[],[], ...
+                    varargin{:} ...
+                );
+            obj_fun_opt_perm = @(param) obj_fun(param,1:k);
+            optimoutput.perm_ = 1:k;
+        end
         %disp('-------------------------------------------------------------------')
         %disp('-------------------------------------------------------------------')
         %disp(strcat("Finished gas scalar BEKK(",num2str(p),",",num2str(q),")-",dist," estimation, targeting."))
@@ -121,7 +145,6 @@ for ii = 1:size(x0,2) % Looping through candidate starting points, until one wor
         %disp('-------------------------------------------------------------------')        
     end
 end
-obj_fun_opt_perm = @(param) obj_fun(param,optimoutput.perm_);
     
 % Output Creation----------------------------------------------------------
 [ nLogL, logLcontr, SigmaE, ScaledScore, eparam ] = obj_fun_opt_perm( eparam );
