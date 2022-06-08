2aad2de93f7e3277c9bf0e3cef1cdeed8b36dffc
mstollenwerk <michael.stollenwerk@live.com>
2022-06-08 09:09:37 +0200

    Update makepd.m
    bugfix

--- toolboxes_own/utility/makepd.m
+++ toolboxes_own/utility/makepd.m
@@ -9,10 +9,10 @@ adjustments_made = zeros(N,1);
 for ii = 1:N
     [X,Y] = eig(nonPDmatrix(:,:,ii));
     if any(diag(Y) < 0)
+        adjustments_made(ii) = sum(diag(Y) < 0);
         Y(Y<0) = eps^(1/3);
         y = X*Y*X';
         PDmatrix(:,:,ii) = .5*(y+y');
-        adjustments_made(ii) = sum(diag(Y) < 0);
     else
         PDmatrix(:,:,ii) = nonPDmatrix(:,:,ii);
     end
